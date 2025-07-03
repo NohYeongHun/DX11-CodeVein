71efce3d22f33df68ce7c12d97701ca49cedcfaf
NYH <dkzndk1100@naver.com>
2025-07-03 20:52:24 +0900

    Collider_Manager 기본 생성.

---
+++ Engine/Public/Collider_Manager.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "Base.h"
+
+
+NS_BEGIN(Engine)
+class CCollider_Manager final : public CBase
+{
+private:
+	explicit CCollider_Manager();
+	virtual ~CCollider_Manager() = default;
+
+public:
+	HRESULT Initialize(_uint iNumLevels);
+
+#pragma region ENGINE에 제공
+public:
+	HRESULT Add_Collider_To_Layer(COLLIDERLAYER eColliderLayer, CCollider* pCollider);
+	HRESULT Remove_Collider_To_Layer(COLLIDERLAYER eColliderLayer, CCollider* pCollider);
+
+public:
+	void Priority_Update(); // list에서 소멸.
+	void Update();			// N
+	void Late_Update();
+#pragma endregion
+
+
+
+private:
+	class CGameInstance* m_pGameInstance = { nullptr };
+	_uint m_iNumLevels = { };
+	// 1 Phase 최적화용 list에 담아두기.
+	list<pair<CCollider*, CCollider*>> m_BroadList = {};
+
+	typedef list<CCollider::COLLIDER_INFO> COLLIDERTYPES;
+
+	unordered_map<COLLIDERLAYER, COLLIDERTYPES> m_Colliders = {};
+	//map<, COLLIDERTYPES> m_Colliders = { nullptr };
+
+
+
+
+public:
+	static CCollider_Manager* Create(_uint iNumLevels);
+	virtual void Free();
+
+private:
+};
+NS_END
+
--- Engine/Public/Engine_Enum.h
+++ Engine/Public/Engine_Enum.h
@@ -1,4 +1,4 @@
-#ifndef Engine_Enum_h__
+﻿#ifndef Engine_Enum_h__
 #define Engine_Enum_h__
 
 namespace Engine
@@ -10,6 +10,40 @@ namespace Engine
 	
 	enum class MOUSEKEYSTATE { LB, RB, MB, END	};
 	enum class MOUSEMOVESTATE {	X, Y, Z, END	};
+
+	enum class COLLIDERSHAPE { NONE = 0, AABB, OBB, SPHERE, CAPSULE, END };
+
+	// 콜라이더 타입 별로 배열에 담을 것.
+	enum class COLLIDERLAYER : uint32_t {
+		NONE = 0
+		, PLAYER = 1 << 1
+		, ENEMY = 1 << 2
+		, STATIC = 1 << 3
+		, DYNAMIC = 1 << 4
+		, END
+	};
+
+	enum KEY_CODE
+	{
+		KEY_START = 0,
+
+		Alpha0 = 0x30, Alpha1 = 0x31, Alpha2 = 0x32, Alpha3 = 0x33, Alpha4 = 0x34, Alpha5 = 0x35, Alpha6 = 0x36, Alpha7 = 0x37, Alpha8 = 0x38, Alpha9 = 0x39,
+
+		Q = 'Q', W = 'W', E = 'E', R = 'R', T = 'T', Y = 'Y', U = 'U', I = 'I', O = 'O', P = 'P',
+		A = 'A', S = 'S', D = 'D', F = 'F', G = 'G', H = 'H', J = 'J', K = 'K', L = 'L',
+		Z = 'Z', X = 'X', C = 'C', V = 'V', B = 'B', N = 'N', M = 'M',
+
+		SHIFT = VK_SHIFT, L_SHIFT = VK_LSHIFT, R_SHIFT = VK_RSHIFT, CONTROL = VK_CONTROL, L_CONTROL = VK_LCONTROL, R_CONTROL = VK_RCONTROL,
+		ALT = VK_MENU, L_ALT = VK_LMENU, R_ALT = VK_RMENU,
+
+		RETURN = VK_RETURN, ESCAPE = VK_ESCAPE, SPACE = VK_SPACE, TAB = VK_TAB, KEY_DELETE = VK_DELETE,
+
+		UP = VK_UP, DOWN = VK_DOWN, LEFT = VK_LEFT, RIGHT = VK_RIGHT,
+
+		MOUSE_L = VK_LBUTTON, MOUSE_R = VK_RBUTTON, MOUSE_WHILL = VK_MBUTTON,
+
+		KEY_END
+	};
 }
 
 #endif // Engine_Enum_h__
